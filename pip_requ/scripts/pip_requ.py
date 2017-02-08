2d7560dc36e8eaa76135de211a4d1043e3837edc
Tuomas Suutari <tuomas.suutari@anders.fi>
2017-02-08 16:22:14 +0200

    Add "pip-requ compile-all" command

--- pip_requ/scripts/pip_requ.py
+++ pip_requ/scripts/pip_requ.py
@@ -1,6 +1,7 @@
 from .. import click
 from . import build_wheels
 from . import compile
+from . import compile_all
 from . import sync
 
 
@@ -11,6 +12,7 @@ def main():
 
 main.add_command(build_wheels.main, 'build-wheels')
 main.add_command(compile.cli, 'compile')
+main.add_command(compile_all.main, 'compile-all')
 main.add_command(sync.cli, 'sync')
 
 
