0d49a033746e00980a9a985e0881ff94ce3e47b9
Tuomas Suutari <tuomas.suutari@anders.fi>
2017-03-05 12:07:20 +0200

    setup: Use setup.cfg for setup variables

---
+++ prequ/__init__.py
@@ -0,0 +1,1 @@
+__version__ = '0.180.9.post0.dev0'
--- setup.py
+++ setup.py
@@ -1,46 +1,5 @@
-from setuptools import find_packages, setup
+import setuptools
 
 
-def get_long_description():
-    with open('README.rst') as fp:
-        return fp.read()
-
-
-setup(
-    name='prequ',
-    version='0.180.9.post0.dev0',
-    url='https://github.com/suutari-ai/prequ/',
-    license='BSD',
-    maintainer='Tuomas Suutari',
-    maintainer_email='tuomas.suutari@anders.fi',
-    description="Prequ -- Python requirement handling",
-    long_description=get_long_description(),
-    packages=find_packages(exclude=['tests']),
-    install_requires=[
-        'click>=6',
-        'first',
-        'pyyaml',
-        'six',
-    ],
-    zip_safe=False,
-    entry_points={
-        'console_scripts': [
-            'prequ = prequ.scripts.prequ:main',
-        ],
-    },
-    platforms='any',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Topic :: System :: Systems Administration',
-    ]
-)
+if __name__ == '__main__':
+    setuptools.setup(setup_requires=['setuptools>=34.0'])
